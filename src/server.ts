b71d5ab4e1096c5be4a643bbfbca86673c9b1c96
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2024-03-12 10:05:06 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,19 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
     try{
+
+        // case insensitive - se  abusca for feita por john wick ou John Wick ou JOHN WICK, o registro vai ser retornado nao consulta
+
+        // case sensitive - se buscar por john wick e no banco estiver como John Wick, não vai ser retornado na consulta
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: "insensitive"} }
+        });
+
+        if(movieWithSameTitle){
+            return res.status(409).send({ message: "Já existe um filme cadastrado com esse título"});
+        }
+
         await prisma.movie.create({
             data: {
                 title,
